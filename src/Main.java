a4046bbb4b8e034d8cde539d3e07c92b13c3f194
Semyon Mikhailov <semen7721@gmail.com>
2023-04-18 20:50:25 +0300

    Добавляет комментарии

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,36 @@
 import java.util.Scanner;
 
 public class Main {
+    // Метод для рисования графика функции на экране
+    // calculator - объект класса Calculator, содержащий функцию для вычисления значений
+    // bottomEdge - нижняя граница графика
+    // topEdge - верхняя граница графика
+    // stringCount - количество строк для отображения графика
     public void drawSymbolPlot(Calculator calculator, double bottomEdge, double topEdge, int stringCount) {
-        double step = (topEdge - bottomEdge) / stringCount;
-        double origin = 0;
-        double[] ordinate = new double[stringCount + 1];
+        double step = (topEdge - bottomEdge) / stringCount; // вычисляем шаг между значениями аргумента функции
+        double origin = 0; // начало координат графика
+        double[] ordinate = new double[stringCount + 1]; // массив значений функции для каждого значения аргумента
         for (int i = 0; i <= stringCount; ++i){
-            double x = bottomEdge + i * step;
-            double f = calculator.func(x);
-            origin = Math.min(f, origin);
-            ordinate[i] = (int) f;
+            double x = bottomEdge + i * step; // вычисляем значение аргумента функции
+            double f = calculator.func(x); // вычисляем значение функции для данного аргумента
+            origin = Math.min(f, origin); // обновляем начало координат графика, если значение функции меньше текущего начала координат
+            ordinate[i] = (int) f; // сохраняем значение функции в массив
         }
-        for (double el: ordinate){
-            for (int i = 0; i < el - origin; i++){
+        for (double el: ordinate){ // проходим по массиву значений функции
+            for (int i = 0; i < el - origin; i++){ // выводим пробелы для выравнивания графика по вертикали
                 System.out.print(' ');
             }
-            System.out.println('*');
+            System.out.println('*'); // выводим символ * для обозначения точки на графике
         }
     }
 
-
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        Main symbol = new Main();
-        Scanner scanner = new Scanner(System.in);
-        double bottomEdge = scanner.nextDouble();
-        double topEdge = scanner.nextDouble();
-        int stringCount = scanner.nextInt();
-        symbol.drawSymbolPlot(calculator, bottomEdge, topEdge, stringCount);
+        Calculator calculator = new Calculator(); // создаем объект класса Calculator, содержащий функцию для вычисления значений
+        Main symbol = new Main(); // создаем объект класса Main, содержащий метод для рисования графика
+        Scanner scanner = new Scanner(System.in); // создаем объект класса Scanner для считывания входных параметров
+        double bottomEdge = scanner.nextDouble(); // считываем нижнюю границу графика
+        double topEdge = scanner.nextDouble(); // считываем верхнюю границу графика
+        int stringCount = scanner.nextInt(); // считываем количество строк для отображения графика
+        symbol.drawSymbolPlot(calculator, bottomEdge, topEdge, stringCount); // вызываем метод для рисования графика
     }
 }
